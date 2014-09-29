f6a8337aac87e344fba3eebdf2f212c49f294aa5
Nikho43 <nicolasbrice@gmail.com>
2014-09-29 16:41:16 -0400

    Create index.php
    page d'introduction du site internet

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<?php include("header.php"); ?>
+</head>
+
+<body>
+
+<h1>NikolasBricePhotography
+</h1>
+
+<?php include("menu.php"); ?> 
+
+
+<section class="section1">
+
+    <h3>Actualite</h3> </br>
+ 
+    <p class="seultexte">Inter has ruinarum varietates a Nisibi quam tuebatur accitus 
+    Vrsicinus, cui nos obsecuturos iunxerat imperiale praeceptum, dispicere litis exitialis 
+    certamina cogebatur abnuens et reclamans, adulatorum oblatrantibus turmis, bellicosus sane 
+    milesque semper et militum ductor sed forensibus iurgiis longe discretus, qui metu sui 
+    discriminis anxius cum accusatores quaesitoresque subditivos sibi consociatos ex isdem 
+    foveis cerneret emergentes, quae clam palamve agitabantur, occultis Constantium litteris
+     edocebat inplorans subsidia, quorum metu tumor notissimus Caesaris exhalaret.</p>
+
+    
+</section>
+
+<center>
+<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FCeletNikoamontreal%2F&amp;width=600&amp;height=290&amp;colorscheme=dark&amp;show_faces=true&amp;header=true&amp;stream=false&amp;show_border=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:600px; height:290px;" allowTransparency="true"></iframe>
+</center>
+
+
+
+<?php include("footer.php"); ?>
+
+</body>
+
+
+
+</html>
